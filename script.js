2ddf857d1af963f0d87992b8616e158fdb9ee4a8
m-j0401 <mehul.jain2206@gmail.com>
2026-02-20 19:52:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+
+let container=document.getElementById("grid-container");
+let color_inp=document.getElementById("color-id");
+let btn=document.querySelector("button");
+let fragment=document.createDocumentFragment();
+for(let i=0;i<9;i++)
+	{
+		let div=document.createElement("div");
+		div.id=i+1;
+		div.className="grid-item";
+		fragment.appendChild(div);
+	}
+container.appendChild(fragment);
+let grid=document.querySelectorAll(".grid-item");
+btn.addEventListener("click",()=>{
+	let v=color_inp.value;
+	for(let g of grid)
+		{
+			g.style.backgroundColor=v;
+		}
+})
